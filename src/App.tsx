a2e99d1619fb67c9189c67b6a31e51fc03acab36
samuelOsborne <samu.osborne@gmail.com>
2023-07-16 07:18:32 +0200

    feat: added jetbrains font

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,27 @@ function App() {
       <div className="card">
         <div className="form-control">
           <label className="label cursor-pointer">
-            <span className="label-text">You've got this thing to do </span>
+            <span className="label-text">Change font to jetbrains</span>
             <input type="checkbox" className="ml-5 checkbox checkbox-primary" />
           </label>
           <label className="label cursor-pointer">
-            <span className="label-text">You've got this thing to do </span>
+            <span className="label-text">Integrate supabase</span>
             <input type="checkbox" className="ml-5 checkbox checkbox-primary" />
           </label>
           <label className="label cursor-pointer">
-            <span className="label-text">You've got this thing to do </span>
+            <span className="label-text">Add login</span>
             <input type="checkbox" className="ml-5 checkbox checkbox-primary" />
           </label>
           <label className="label cursor-pointer">
-            <span className="label-text">You've got this thing to do </span>
+            <span className="label-text">Read / Write to-do's to supabase</span>
             <input type="checkbox" className="ml-5 checkbox checkbox-primary" />
           </label>
           <label className="label cursor-pointer">
-            <span className="label-text">You've got this thing to do </span>
+            <span className="label-text">Create lemonsqueezy shop</span>
             <input type="checkbox" className="ml-5 checkbox checkbox-primary" />
           </label>
           <label className="label cursor-pointer">
-            <span className="label-text">You've got this thing to do You've got this thing to do You've got this thing to do You've got this thing to do You've got this thing to do You've got this thing to do </span>
+            <span className="label-text">Integrate lemonsqueezy license key paywall</span>
             <input type="checkbox" className="ml-5 checkbox checkbox-primary" />
           </label>
         </div>
